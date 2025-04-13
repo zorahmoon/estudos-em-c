8abe4f8683341d7f63fb0ba187fdb61201dc8920
Ester Miranda Cobos <ester.miranda.cobos@gmail.com>
2025-04-13 18:28:35 -0300

    idade de usuário

---
+++ modulo-01-introducao/02.IdadeUsuario.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int idade;
+    
+    printf("Quantos anos você tem? ");
+    scanf("%d", &idade);
+    
+    printf("Legal, você tem %d aninhos! 🥹\n", idade);
+    
+    return 0;
+}
